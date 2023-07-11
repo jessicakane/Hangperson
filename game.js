273a05796c6b557b2816062241c1c3fd6c41345a
evgenyrinkevich <rinkevich2000@gmail.com>
2023-07-11 15:43:40 +0300

    added lives count div in games.html

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 const letters = document.querySelectorAll('.image-fluid');
 const questionElement = document.querySelector('#question');
 const answerElement = document.querySelector('#answer');
+const livesElement = document.querySelector('#lives');
 
 const category = 'hightech';
+const livesLeft = 10;
 
 const fetchQuestions = async (path) => {
   const res = await fetch(path);
@@ -16,6 +18,10 @@ const getQuestion = (category, data) => {
   return categoryData[index];
 };
 
+const handleLetterClick = (letter) => {
+  if (livesLeft-- === 0) console.log('you lost');
+};
+
 letters.forEach((letterEl) =>
   letterEl.addEventListener('click', () =>
     console.log(letterEl.dataset.letter, 'is clicked')
@@ -26,6 +32,7 @@ window.addEventListener('load', async () => {
   const data = await fetchQuestions('./data.json');
   const { question, hint, answer } = getQuestion(category, data);
 
+  livesElement.innerText = `Lives: ${livesLeft}`;
   questionElement.innerText = question;
   const answerHTML = `<img src="./images/input_square.png"/>`.repeat(
     answer.length
