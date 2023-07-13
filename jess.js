3d340ee5f8a80337244949b0d9755e34dd96be26
jessicakane <jessicakane213@gmail.com>
2023-07-13 12:03:25 +0300

    talking bubble, chalkboard, some other styling

---
+++ jess.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    buttons.forEach((btn) => {
+      btn.classList.remove('active');
+    });
+    button.classList.add('active');
+  });
+});
